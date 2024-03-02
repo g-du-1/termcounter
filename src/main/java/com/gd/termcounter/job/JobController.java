397d5b8c3bbefd716ae9d23ce101040341945a70
g-du-1 <dudaszeg1@gmail.com>
2024-03-02 17:31:27 +0000

    Add JobController

---
+++ src/main/java/com/gd/termcounter/job/JobController.java
@@ -0,0 +1,25 @@
+package com.gd.termcounter.job;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/jobs")
+public class JobController {
+
+    private final JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Job> createJob(@RequestBody Job job) {
+        Job createdJob = jobService.saveJob(job);
+
+        return ResponseEntity.ok(createdJob);
+    }
+}
---
+++ src/test/java/com/gd/termcounter/job/JobControllerTest.java
@@ -0,0 +1,39 @@
+package com.gd.termcounter.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(JobController.class)
+public class JobControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JobService jobService;
+
+    @Test
+    public void testCreateJob() throws Exception {
+        Job job = new Job();
+        job.setTitle("Software Engineer");
+
+        when(jobService.saveJob(any(Job.class))).thenReturn(job);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/jobs/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"Software Engineer\"}")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").value("Software Engineer"));
+    }
+}
